3293dc80f097919feef3a622663fc4fa08786921
RReiso <rutareiso@protonmail.com>
2021-06-16 18:03:23 -0400

    Add flipCard and checkForMatch functions

--- Memory-game/script.js
+++ Memory-game/script.js
@@ -1,32 +1,52 @@
-  const cards = [
-    {id:1,
-    img: "./images/1.png"},
-    {id:2,
-    img: "./images/2.png"}
-  ]
+const cards = [
+	{ img: "initial", src: "./images/0.png" },
+	{ img: 1, src: "./images/1.jpeg" },
+	{ img: 2, src: "./images/1.jpeg" },
+	{ img: 3, src: "./images/2.jpeg" },
+	{ img: 4, src: "./images/2.jpeg" },
+	{ img: 5, src: "./images/3.jpeg" },
+	{ img: 6, src: "./images/3.jpeg" },
+	{ img: 7, src: "./images/4.jpeg" },
+	{ img: 8, src: "./images/4.jpeg" },
+	{ img: 9, src: "./images/5.jpeg" },
+	{ img: 10, src: "./images/5.jpeg" },
+];
+let flippedCards = [];
 
-function flipCard (){
-	const dataID = this.getAttribute("data-id");
-	this.setAttribute("src", "./images/2.png");
-};
+function flipCard() {
+  if (clickEnabled === true){
+    const dataID = this.getAttribute("data-id");
+    this.setAttribute("src", cards[dataID].src);
+    flippedCards.push(cards[dataID].src);
+    if (flippedCards.length === 2) {
+      clickEnabled = false;
+      setTimeout(checkForMatch, 600);
+    }
+	}
+}
 
- const grid = document.querySelector(".grid") ;
+function checkForMatch() {
+	if (flippedCards[0] !== flippedCards[1]) {
+		const firstCard = document.querySelector(`img[src="${flippedCards[0]}"`);
+    const secondCard = document.querySelector(`img[src="${flippedCards[1]}"`);
+		setInitialBackground(firstCard);
+    setInitialBackground(secondCard);
+	}
+	flippedCards = [];
+  clickEnabled = true;
+}
 
- for (let i =1; i<17; i++){
-   let card = document.createElement("img");
-   card.setAttribute("src", "./images/1.png");
-   card.setAttribute("data-id",i);
-   grid.appendChild(card);
-   card.addEventListener("click", flipCard);
- }
+const grid = document.querySelector(".grid");
+let clickEnabled = true;
 
- 
+for (let i = 1; i < 17; i++) {
+	let card = document.createElement("img");
+	setInitialBackground(card);
+	card.setAttribute("data-id", i);
+	grid.appendChild(card);
+	card.addEventListener("click", flipCard);
+}
 
- 
-
-
-
-
- 
- 
- 
+function setInitialBackground(card){
+  card.setAttribute("src", "./images/0.png");
+}
